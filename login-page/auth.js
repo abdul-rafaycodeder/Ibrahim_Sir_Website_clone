81473f3a8056633f52f9c2159c5fe5f969a24d82
Abdul Rafay <abdulrafaycodeder@gmail.com>
2026-03-07 06:04:21 +0500

    coding

--- login-page/auth.js
+++ login-page/auth.js
@@ -2,40 +2,10 @@ function switchForm() {
     document.getElementById("loginForm").classList.toggle("active");
     document.getElementById("registerForm").classList.toggle("active");
 }
- 
+
 // Demo Submit
 document.querySelectorAll("form").forEach(form => {
     form.addEventListener("submit", function (e) {
         e.preventDefault();
-        alert("Form Submitted (Backend not connected)");
     });
 });
-
-//-------------------------User Authentication-------------------//
-
-
-// import { initializeApp } from "https://www.gstatic.com/firebasejs/12.10.0/firebase-app.js";
-// import {
-//     getAuth,
-//     createUserWithEmailAndPassword
-// } from "https://www.gstatic.com/firebasejs/12.10.0/firebase-auth.js";
-
-
-// const firebaseConfig = {
-//     apiKey: "AIzaSyA8DIvSCEDnYWzBeic7Dx25bkUcfhBlx3A",
-//     authDomain: "personal-ai-clone-ibrahim-sir.firebaseapp.com",
-//     projectId: "personal-ai-clone-ibrahim-sir",
-//     storageBucket: "personal-ai-clone-ibrahim-sir.firebasestorage.app",
-//     messagingSenderId: "74129868246",
-//     appId: "1:74129868246:web:1472d8571943b9876e9077"
-// };
-
-// // Initialize Firebase
-// const app = initializeApp(firebaseConfig);
-// const auth = getAuth(app);
-
-
-
-// function signUp(){
-
-// }
---
+++ login-page/auth1.js
@@ -0,0 +1,55 @@
+// -------------------------------------------------Sign Up------------------------------------------//
+
+import { initializeApp } from "https://www.gstatic.com/firebasejs/12.10.0/firebase-app.js";
+import {
+    getAuth,
+    createUserWithEmailAndPassword
+} from "https://www.gstatic.com/firebasejs/12.10.0/firebase-auth.js";
+
+
+const firebaseConfig = {
+    apiKey: "AIzaSyA8DIvSCEDnYWzBeic7Dx25bkUcfhBlx3A",
+    authDomain: "personal-ai-clone-ibrahim-sir.firebaseapp.com",
+    projectId: "personal-ai-clone-ibrahim-sir",
+    storageBucket: "personal-ai-clone-ibrahim-sir.firebasestorage.app",
+    messagingSenderId: "74129868246",
+    appId: "1:74129868246:web:1472d8571943b9876e9077"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+const auth = getAuth(app);
+
+const signUpButton = document.getElementById('signUpBtn');
+
+signUpButton.addEventListener('click', signUp);
+
+function signUp() {
+    // const SignUpName = document.getElementById('signUpFullName').value
+    const SignUpemail = document.getElementById('signUpEmale').value
+    const SignUpPassWord = document.getElementById('signUpPassword').value
+
+    createUserWithEmailAndPassword(auth, SignUpemail, SignUpPassWord)
+        .then((userCredential) => {
+            // Signed up 
+            const user = userCredential.user;
+
+            if (user) {
+                alert("Create Account Successfully")
+
+                SignUpemail = ''
+                SignUpPassWord = ''
+            }
+        })
+        .catch((error) => {
+            const errorCode = error.code;
+            const errorMessage = error.message;
+            if (errorCode) {
+                alert('Weak password')
+            }
+        });
+}
+
+// -------------------------------------------------Sign In------------------------------------------//
+
+
