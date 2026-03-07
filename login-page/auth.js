a7c68e75417496b3f50ef0fee7ef340fb4aa7951
Abdul Rafay <abdulrafaycodeder@gmail.com>
2026-03-07 05:35:40 +0500

    coding

--- login-page/auth.js
+++ login-page/auth.js
@@ -14,25 +14,25 @@ document.querySelectorAll("form").forEach(form => {
 //-------------------------User Authentication-------------------//
 
 
-import { initializeApp } from "https://www.gstatic.com/firebasejs/12.10.0/firebase-app.js";
-import {
-    getAuth,
-    createUserWithEmailAndPassword
-} from "https://www.gstatic.com/firebasejs/12.10.0/firebase-auth.js";
+// import { initializeApp } from "https://www.gstatic.com/firebasejs/12.10.0/firebase-app.js";
+// import {
+//     getAuth,
+//     createUserWithEmailAndPassword
+// } from "https://www.gstatic.com/firebasejs/12.10.0/firebase-auth.js";
 
 
-const firebaseConfig = {
-    apiKey: "AIzaSyA8DIvSCEDnYWzBeic7Dx25bkUcfhBlx3A",
-    authDomain: "personal-ai-clone-ibrahim-sir.firebaseapp.com",
-    projectId: "personal-ai-clone-ibrahim-sir",
-    storageBucket: "personal-ai-clone-ibrahim-sir.firebasestorage.app",
-    messagingSenderId: "74129868246",
-    appId: "1:74129868246:web:1472d8571943b9876e9077"
-};
+// const firebaseConfig = {
+//     apiKey: "AIzaSyA8DIvSCEDnYWzBeic7Dx25bkUcfhBlx3A",
+//     authDomain: "personal-ai-clone-ibrahim-sir.firebaseapp.com",
+//     projectId: "personal-ai-clone-ibrahim-sir",
+//     storageBucket: "personal-ai-clone-ibrahim-sir.firebasestorage.app",
+//     messagingSenderId: "74129868246",
+//     appId: "1:74129868246:web:1472d8571943b9876e9077"
+// };
 
-// Initialize Firebase
-const app = initializeApp(firebaseConfig);
-const auth = getAuth(app);
+// // Initialize Firebase
+// const app = initializeApp(firebaseConfig);
+// const auth = getAuth(app);
 
 
 
