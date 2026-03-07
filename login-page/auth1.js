5790c394c58a289867cd2c582809dde4a4bb0f9b
Abdul Rafay <abdulrafaycodeder@gmail.com>
2026-03-07 21:58:41 +0500

    coding nice hai

--- login-page/auth1.js
+++ login-page/auth1.js
@@ -1,5 +1,8 @@
 // -------------------------------------------------Sign Up------------------------------------------//
 
+import dotenv from "dotenv"
+dotenv.config()
+
 import { initializeApp } from "https://www.gstatic.com/firebasejs/12.10.0/firebase-app.js";
 import {
     getAuth,
